31015cd865244ab10ff0ea361cadc9d5f1abb38e
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-04-02 16:13:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
 
+  def initialize(discount={})
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  attr_accessor :total, :discount, :last_trans_amount, :items
+
+  def add_item(title, price, quantity=nil)
+    previous = @total
+    @items << title
+    if quantity != nil
+      @total += price * quantity
+    else
+      @total += price
+    end
+  end
+
+  def apply_discount
+    total = add_item
+    total = total - (discount * total)
+  end
+
+end
